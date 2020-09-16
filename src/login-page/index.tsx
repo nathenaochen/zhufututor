36e6524db4dd96a26e73a547fbd29905da885306
Nathen <13147177623@163.com>
2020-09-16 14:18:56 +0800

    修改登录页逻辑

--- src/login-page/index.tsx
+++ src/login-page/index.tsx
@@ -6,27 +6,63 @@ import styles from './lp.less';
 
 function LoginPage(){
 
-  // const []
+  //用户账号
+  const [userAccount, setUserAccount] = useState('');
+  //用户密码
+  const [pwd, setPwd] = useState('');
+  //用户密码--隐藏
+  const [pwdNo, setPwdNo] = useState('');
+  //是否隐藏密码----flutter的webview支持input的password有bug，先自己手动实现此功能
+  const [isshowPwd, setIsshowPwd] = useState(true);
 
   //去登陆
   async function gotoLogin(){
-    JSSDK.openWebview({url:'/',title:'new webview'})
+    console.log(userAccount,pwd);
+    // JSSDK.openWebview({url:'/',title:'new webview'})
+  }
+
+  function handleChange(e:React.ChangeEvent<HTMLInputElement>){
+    // console.log(e);
+    let value = e.target.value;
+    // setPwd((pwd)=>{
+    //   return pwd + value.slice((value.length - 1));
+    // });
+    // setPwdNo((pwd)=>{
+    //   return pwd + '*';
+    // });
+    setPwd(value);
+  }
+
+  function handleKeydown(e: React.KeyboardEvent<HTMLInputElement>){
+    let value = e.key;
+    setPwd((pwd)=>{
+      return pwd + value;
+    });
+    setPwdNo((pwd)=>{
+      return pwd + '*';
+    });
   }
 
   return (
     <div>
       <div className={styles['login-box']}>
-        {/* <p className={styles['login-type']}>密码登陆</p> */}
         <ul>
           <li className={styles['login-item']}>
             <span></span>
-            <input type="tel" placeholder='请输入账号'/>
+            <input 
+              type="tel" placeholder='请输入账号' value={userAccount} 
+              onChange={(e:React.ChangeEvent<HTMLInputElement>)=>{setUserAccount(e.target.value)}}
+            />
             <span></span>
           </li>
           <li className={styles['login-item']}>
             <span></span>
-            <input type="url" placeholder='请输入密码'/>
-            <span></span>
+            <input 
+              type="password" placeholder='请输入密码' value={pwd} 
+              onChange={(e:React.ChangeEvent<HTMLInputElement>)=>{handleChange(e)}}
+              // onKeyDown={(e: React.KeyboardEvent<HTMLInputElement>)=>{console.log(e,e.key,e.keyCode);handleKeydown(e);}}
+            />
+            <span onClick={(e:React.MouseEvent)=>{e.stopPropagation();setIsshowPwd((isshow)=>{return !isshow})}} className={isshowPwd ? '' : styles['see']}></span>
           </li>
         </ul>
         <div className={styles['options']}>
@@ -34,8 +70,14 @@ function LoginPage(){
           <p>手机登陆</p>
         </div>
         <div className={styles['button-box']}>
-          <p className={cns(styles['login-button'],styles['first'])} onClick={()=>{}}>登陆</p>
-          <p className={cns(styles['login-button'],styles['second'])}>注册</p>
+          <p 
+            className={cns(styles['login-button'],styles['first'])} 
+            onClick={(e:React.MouseEvent<HTMLParagraphElement, MouseEvent>)=>{e.stopPropagation();gotoLogin()}}
+          >登陆</p>
+          <p 
+            className={cns(styles['login-button'],styles['second'])} 
+            onClick={(e:React.MouseEvent<HTMLParagraphElement, MouseEvent>)=>{}}
+          >注册</p>
         </div>
       </div>
     </div>
